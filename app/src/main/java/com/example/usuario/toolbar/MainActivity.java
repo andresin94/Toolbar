a0cff2d3a2b8bf4e701c4b7270739f2b21b70c63
andres <andresim28@gmail.com>
2018-04-09 14:18:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/usuario/toolbar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.usuario.toolbar;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar= (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);  //El toolbar sera actionbar
+        
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
